123fbc46a204424337fd67d8feb3e5dcadc4774c
freaklucas <lucas.oliveira.freak@gmail.com>
2023-08-21 22:54:02 -0300

    feat:populando bd

---
+++ SmartSchool/Data/SmartContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.Models;
+
+namespace SmartSchool.Data {
+  public class SmartContext : DbContext {
+    public SmartContext(DbContextOptions <SmartContext> options) : base(options) {}
+    public DbSet <Student> Students { get; set; }
+    public DbSet <Teacher> Teachers { get; set; }
+    public DbSet <Discipline> Disciplines { get; set; }
+    public DbSet <StudentDiscipline> StudentsDisciplines { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+      builder.Entity<StudentDiscipline>().HasKey(AD => new {AD.StudentId, AD.DisciplineId}); // relacao mts pra mts
+
+    }
+
+  }
+}
