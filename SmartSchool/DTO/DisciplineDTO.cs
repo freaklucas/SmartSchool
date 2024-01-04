820b08ef8b511b55ee3c73107e1e1097647ee76e
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-03 23:08:48 -0300

    feat:DTO para para Disciplina e professor

---
+++ SmartSchool/DTO/DisciplineDTO.cs
@@ -0,0 +1,17 @@
+﻿using SmartSchool.Models;
+
+namespace SmartSchool.DTO;
+public class DisciplineDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CargaHoraria { get; set; }
+    public int TeacherId { get; set; }
+    public int? PreRequisitId { get; set; } = null;
+    public Discipline PreRequisit { get; set; }
+    public Teacher Teacher { get; set; }
+    public int CourseId { get; set; }
+    public Course Course { get; set; }
+
+    public IEnumerable<StudentDiscipline> StudentsDisciplines { get; set; }
+}
---
+++ SmartSchool/DTO/TeacherRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using SmartSchool.Models;
+
+namespace SmartSchool.DTO;
+public class TeacherRegisterDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? NumberPhone { get; set; }
+    public int Registration { get; set; }
+    public bool Ativo { get; set; } = true;
+
+    public IEnumerable<Discipline>? Disciplines { get; set; }
+}
