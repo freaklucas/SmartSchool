496325ff4ad15f2649f1d45e1dd1ffd58ee0193a
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-05 22:54:03 -0300

    refact: minimizando DTO de estudante

--- SmartSchool/DTO/StudentDTO.cs
+++ SmartSchool/DTO/StudentDTO.cs
@@ -3,13 +3,8 @@ namespace SmartSchool.DTO;
 public class StudentDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string LastName { get; set; }
+    public string FullName { get; set; }
     public string? NumberPhone { get; set; }
-    public int Registration { get; set; }
-    public DateTime BirthDate { get; set; }
-    public DateTime InitDate { get; set; }
-    
     public bool Ativo { get; set; }
 
 }
